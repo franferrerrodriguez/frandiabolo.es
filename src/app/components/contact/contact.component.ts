baad6d36c0f45c14aab1c0a43915dd71e1c5dcff
Francisco José Ferrer Rodríguez <francisco.ferrer@sngular.com>
2023-12-14 17:50:16 +0100

    feat(contact): implement contact form endpoint logic

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,24 +1,37 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+} from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'contact',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ContactComponent {
   form!: FormGroup;
+  submit: boolean = false;
+  success: boolean = false;
+  error: boolean = false;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     this.form = this.fb.group({
@@ -28,6 +41,35 @@ export class ContactComponent {
   }
 
   onSubmit() {
-    console.log('Formulario enviado:', this.form.value, this.form.valid);
+    this.submit = true;
+
+    this.http
+      .post('https://formspree.io/f/xnqkekwn', this.form.value)
+      .pipe(
+        finalize(() => {
+          this.submit = false;
+          this.cdr.detectChanges();
+        })
+      )
+      .subscribe({
+        next: () => {
+          this.showToast(true);
+        },
+        error: (error) => {
+          this.showToast(false);
+        },
+      });
+  }
+
+  showToast(success: boolean) {
+    if (success) {
+      this.success = true;
+    } else {
+      this.error = true;
+    }
+    setTimeout(() => {
+      this.success = false;
+      this.cdr.detectChanges();
+    }, 3000);
   }
 }
